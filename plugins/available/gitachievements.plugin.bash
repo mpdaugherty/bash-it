6b56651d0af53daacca8bd01ca4d17c5ec72d9e7
Michael P. Daugherty <mike@mpdaugherty.com>
2012-12-30 11:09:23 +0800

    Adding a plugin for git-achievements

---
+++ plugins/available/gitachievements.plugin.bash
@@ -0,0 +1,8 @@
+cite about-plugin
+about-plugin 'Set up Git Achievements; requires it to be installed'
+
+# 1. Fork https://github.com/icefox/git-achievements to your Github repository
+# 2. Clone it to ~/.bin/git-achievements
+
+export PATH=$PATH:$HOME/.bin/git-achievements
+alias git='git-achievements'
