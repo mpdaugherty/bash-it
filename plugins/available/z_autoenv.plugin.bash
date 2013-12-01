2c4a81cd4129b9e96245f3383ca423337cfbad74
Michael P. Daugherty <mike@mpdaugherty.com>
2013-11-30 19:57:35 -0800

    Forcibly turn off the auto-env reload.  This screwed up auto-loading of .ruby-version files by RVM, and I do not care enough about this bash_it plugin to investigate more.

--- plugins/available/z_autoenv.plugin.bash
+++ plugins/available/z_autoenv.plugin.bash
@@ -8,38 +8,5 @@ fi
 
 autoenv_init()
 {
-  typeset target home _file
-  typeset -a _files
-  target=$1
-  home="$(dirname $HOME)"
 
-  _files=( $(
-    while [[ "$PWD" != "/" && "$PWD" != "$home" ]]
-    do
-      _file="$PWD/.env"
-      if [[ -e "${_file}" ]]
-      then echo "${_file}"
-      fi
-      builtin cd ..
-    done
-  ) )
-
-  _file=${#_files[@]}
-  while (( _file > 0 ))
-  do
-    source "${_files[_file-__array_offset]}"
-    : $(( _file -= 1 ))
-  done
-}
-
-cd()
-{
-  if builtin cd "$@"
-  then
-    autoenv_init
-    return 0
-  else
-    echo "else?"
-    return $?
-  fi
 }
